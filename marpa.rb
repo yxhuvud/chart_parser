06bf43510a3eafd59eede714bc0cf49f94a40f31
Linus <sellberg@gmail.com>
2014-04-18 12:03:11 +0200

    Save all charts in an array.

--- marpa.rb
+++ marpa.rb
@@ -2,16 +2,17 @@ require 'set'
 
 class Marpa
   attr_accessor :chart, :source, :state_machine, :state_size
-  attr_accessor :previous_chart
+  attr_accessor :previous_chart, :charts
 
   def initialize grammar
     @state_machine = StateMachine.new(grammar)
     @state_size = state_machine.size
+    @charts = []
     reset
   end
 
   def reset
-    @chart = Chart.new(0, state_size)
+    charts[0] = @chart = Chart.new(0, state_size)
     @previous_chart = nil
     chart.add state_machine.starting_state, @chart
     self
@@ -20,19 +21,17 @@ class Marpa
   def parse source
     @source = source
     consumer = method(:marpa_pass)
-    if source.kind_of?(Enumerator)
-      source.each &consumer
-    else
-      source.each_char &consumer
-    end
+    generator = source.kind_of?(Enumerator) ?  source : source.each_char
+    generator.each.with_index &consumer
     success?
   end
 
-  def marpa_pass sym
+  def marpa_pass sym, index
     @previous_chart = chart
     @previous_chart.memoize_transitions
 
     @chart = Chart.new(@previous_chart.index.succ, state_size)
+    charts[index + 1] = chart
     consume sym
   end
   
