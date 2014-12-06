d124d32504cbe566e695fc88afebab9af5406d1e
Linus Sellberg <sellberg@gmail.com>
2014-12-06 22:04:08 +0100

    Move examples dir from spec dir.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require 'rspec/its'
 require 'rspec/collection_matchers'
+
 RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = [:should, :expect]
@@ -10,8 +11,8 @@ require File.expand_path('./lib/chart_parser.rb')
 
 include ChartParser
 
-require 'examples/palindrome'
-require 'examples/a_palindrome'
-require 'examples/ambigous_a'
-require 'examples/right_recursive'
-require 'examples/left_recursive'
+require File.expand_path('./examples/palindrome')
+require File.expand_path('./examples/a_palindrome')
+require File.expand_path('./examples/ambigous_a')
+require File.expand_path('./examples/right_recursive')
+require File.expand_path('./examples/left_recursive')
